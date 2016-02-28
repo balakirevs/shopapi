acba35be0ba13f5f4e53bf74454642b81a51c8bb
Aleksandr Balakiriev <balakirevs@i.ua>
2016-02-28 13:20:48 +0100

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do      
-      resources :users, :only => [:show, :create, :update, :destroy] do
-      resources :products, :only => [:create, :update, :destroy]
-      resources :orders, :only => [:index, :show, :create]
+      resources :users, only: [:show, :create, :update, :destroy] do
+      resources :products, only: [:create, :update, :destroy]
+      resources :orders, only: [:index, :show, :create]
     end
 
-    resources :sessions, :only => [:create, :destroy]
-    resources :products, :only => [:show, :index]
+    resources :sessions, only: [:create, :destroy]
+    resources :products, only: [:show, :index]
     end
   end
 end
