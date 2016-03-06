0d873b8f254c52ada891732190483329969e8abb
Aleksandr Balakiriev <balakirevs@i.ua>
2016-03-06 23:54:55 +0100

    Removed duplication & update rails version

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -11,50 +11,47 @@ RSpec.describe Order, type: :model do
 	  it { should belong_to :user }
 	  it { should have_many(:placements) }
   	it { should have_many(:products).through(:placements) }
-	end
 
-	describe '#set_total!' do
-    before(:each) do
-      product_1 = FactoryGirl.create :product, price: 100
-      product_2 = FactoryGirl.create :product, price: 85
-      placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
-      placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
-      @order = FactoryGirl.build :order
-      @order.placements << placement_1
-      @order.placements << placement_2
+  	describe '#set_total!' do
+      before(:each) do
+        product_1 = FactoryGirl.create :product, price: 100
+        product_2 = FactoryGirl.create :product, price: 85
+        placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
+        placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
+        order.placements << placement_1
+        order.placements << placement_2
+      end
+
+      it "returns the total amount to pay for the products" do
+        expect{order.set_total!}.to change{order.total.to_f}.from(0).to(1575)
+      end
     end
 
-    it "returns the total amount to pay for the products" do
-      expect{@order.set_total!}.to change{@order.total.to_f}.from(0).to(1575)
-    end
-  end
+  	describe "#build_placements_with_product_ids_and_quantities" do
+      before(:each) do
+        product_1 = FactoryGirl.create :product, price: 100, quantity: 5
+        product_2 = FactoryGirl.create :product, price: 85, quantity: 10
+        @product_ids_and_quantities = [[product_1.id, 2], [product_2.id, 3]]
+      end
 
-	describe "#build_placements_with_product_ids_and_quantities" do
-    before(:each) do
-      product_1 = FactoryGirl.create :product, price: 100, quantity: 5
-      product_2 = FactoryGirl.create :product, price: 85, quantity: 10
-      @order = FactoryGirl.build :order
-      @product_ids_and_quantities = [[product_1.id, 2], [product_2.id, 3]]
-    end
-
-    it "builds 2 placements for the order" do
-      expect{@order.build_placements_with_product_ids_and_quantities(@product_ids_and_quantities)}.to change{@order.placements.size}.from(0).to(2)
-    end
-  end
-
-  describe "#valid?" do
-    before do
-      product_1 = FactoryGirl.create :product, price: 100, quantity: 5
-      product_2 = FactoryGirl.create :product, price: 85, quantity: 10
-      placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
-      placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
-      @order = FactoryGirl.build :order
-      @order.placements << placement_1
-      @order.placements << placement_2
+      it "builds 2 placements for the order" do
+        expect{order.build_placements_with_product_ids_and_quantities(@product_ids_and_quantities)}.to change{order.placements.size}.from(0).to(2)
+      end
     end
 
-    it "becomes invalid due to insufficient products" do
-      expect(@order).to_not be_valid
+    describe "#valid?" do
+      before do
+        product_1 = FactoryGirl.create :product, price: 100, quantity: 5
+        product_2 = FactoryGirl.create :product, price: 85, quantity: 10
+        placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
+        placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
+        order.placements << placement_1
+        order.placements << placement_2
+      end
+
+      it "becomes invalid due to insufficient products" do
+        expect(order).to_not be_valid
+      end
     end
   end
 end
