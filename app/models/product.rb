a1efa536a0f95fa1b87ac76ffbef930501b28661
Aleksandr Balakiriev <balakirevs@i.ua>
2017-07-15 23:52:48 +0200

    syntax update

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,21 +7,13 @@ class Product < ActiveRecord::Base
   has_many :placements
   has_many :orders, through: :placements    
 
-  scope :filter_by_title, lambda { |keyword|
-    where("lower(title) LIKE ?", "%#{keyword.downcase}%" ) 
-  }
+  scope :filter_by_title, -> (keyword) { where("lower(title) LIKE ?", "%#{keyword.downcase}%" ) }
 
-  scope :above_or_equal_to_price, lambda { |price| 
-    where("price >= ?", price) 
-  }
+  scope :above_or_equal_to_price, -> (price) { where("price >= ?", price) }
 
-  scope :below_or_equal_to_price, lambda { |price| 
-    where("price <= ?", price) 
-  }
+  scope :below_or_equal_to_price, -> (price) { where("price <= ?", price) }
 
-  scope :recent, -> {
-    order(:updated_at)
-  }
+  scope :recent, -> { order(:updated_at) }
 
   def self.search(params = {})
     products = params[:product_ids].present? ? Product.where(id: params[:product_ids]) : Product.all
