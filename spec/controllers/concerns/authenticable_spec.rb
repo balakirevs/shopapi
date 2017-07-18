dc7eb36bd072268e0938762121bc89e56c40494c
Aleksandr Balakiriev <balakirevs@i.ua>
2017-07-18 07:02:16 +0200

    Adds controller type for concerns spec

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -4,7 +4,7 @@ class Authentication < ActionController::Base
   include Authenticable
 end
 
-describe Authenticable do
+describe Authenticable, type: :controller do
   let(:authentication) { Authentication.new }
   subject { authentication }
 
@@ -14,6 +14,7 @@ describe Authenticable do
       request.headers["Authorization"] = @user.auth_token
       allow(authentication).to receive(:request).and_return(request)
     end
+
     it "returns the user from the authorization header" do
       expect(authentication.current_user.auth_token).to eql @user.auth_token
     end
