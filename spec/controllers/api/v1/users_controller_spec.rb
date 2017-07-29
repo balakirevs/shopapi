49b332182cbe871925facc51730592b667d5f9b4
Aleksandr Balakiriev <balakirevs@i.ua>
2017-07-29 12:03:20 +0200

    Refactors users_controller_spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-
   let(:user) { create(:user) }
 
   describe 'GET #show' do
-    before(:each) do
-      get :show, params: { id: user.id }, format: :json
-    end
+    before(:each) { get :show, params: { id: user.id } }
+
+    let(:user_response) { json_response[:user] }
 
     it 'returns the information about a reporter on a hash' do
-      user_response = json_response[:user][:email]
-      expect(user_response).to eql user.email
+      expect(user_response[:email]).to eql user.email
     end
 
     it 'has the product ids as an embeded object' do
-      user_response = json_response[:user][:product_ids]
-      expect(user_response).to eql []
+      expect(user_response[:product_ids]).to eql []
+    end
+
+    it 'should exclude user token' do
+      expect(user_response[:token]).to be_falsey
     end
 
     it { should respond_with 200 }
   end
 
-  describe "POST #create" do
-
-    context "when is successfully created" do
+  describe 'POST #create' do
+    context 'when is successfully created' do
       before(:each) do
         @user_attributes = attributes_for :user
-        post :create, params: { user: @user_attributes }, format: :json
+        post :create, params: { user: @user_attributes }
       end
 
+      let(:user_response) { json_response[:user] }
+
       it 'renders the json representation for the user record just created' do
-        user_response = json_response[:user][:email]
-        expect(user_response).to eql @user_attributes[:email]
+        expect(user_response[:email]).to eql @user_attributes[:email]
       end
 
       it { should respond_with 201 }
@@ -40,18 +41,18 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
     context 'when is not created' do
       before(:each) do
-        @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" }
-        post :create, params: { user: @invalid_user_attributes }, format: :json
+        @invalid_user_attributes = attributes_for :user, email: nil
+        post :create, params: { user: @invalid_user_attributes }
       end
 
+      let(:user_response) { json_response }
+
       it 'renders an errors json' do
-        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
       it 'renders the json errors on why the user could not be created' do
-        user_response = json_response[:errors][:email]
-        expect(user_response).to include "can't be blank"
+        expect(user_response[:errors][:email]).to include "can't be blank"
       end
 
       it { should respond_with 422 }
@@ -59,36 +60,31 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   end
 
   describe 'PUT/PATCH #update' do
-    before(:each) do
-      api_authorization_header user.auth_token
-    end
+    before(:each) { api_authorization_header user.auth_token }
 
     context 'when is successfully updated' do
-      before(:each) do
-        patch :update, params: { id: user.id, user: { email: "newmail@example.com" } }, format: :json
-      end
+      before(:each) { patch :update, params: { id: user.id, user: { email: 'newmail@example.com' } } }
+
+      let(:user_response) { json_response[:user] }
 
       it 'renders the json representation for the updated user' do
-        user_response = json_response[:user][:email]
-        expect(user_response).to eql 'newmail@example.com'
+        expect(user_response[:email]).to eql 'newmail@example.com'
       end
 
       it { should respond_with 200 }
     end
 
     context 'when is not updated' do
-      before(:each) do
-        patch :update, params: { id: user.id, user: { email: 'bademail.com' } }, format: :json
-      end
+      before(:each) { patch :update, params: { id: user.id, user: { email: 'bademail.com' } } }
+
+      let(:user_response) { json_response }
 
       it 'renders an errors json' do
-        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
       it 'renders the json errors on whye the user could not be created' do
-        user_response = json_response[:errors][:email]
-        expect(user_response).to include 'is invalid'
+        expect(user_response[:errors][:email]).to include 'is invalid'
       end
 
       it { should respond_with 422 }
@@ -98,7 +94,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   describe 'DELETE #destroy' do
     before(:each) do
       api_authorization_header user.auth_token
-      delete :destroy, params: { id: user.id }, format: :json
+      delete :destroy, params: { id: user.id }
     end
 
     it { should respond_with 204 }
