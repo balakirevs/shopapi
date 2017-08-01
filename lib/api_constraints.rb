6b7722cff0805809d75f67a4322dc17deb63c693
Aleksandr Balakiriev <balakirevs@i.ua>
2017-08-01 12:50:03 +0200

    Fixes api constrains test

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -5,6 +5,6 @@ class ApiConstraints
   end
 
   def matches?(req)
-    @default || req.headers['Accept'].include?("application/vnd.shop_api.v#{@version}")
+    @default || req.headers['Accept'].include?("application/vnd.shopapi.v#{@version}")
   end
 end
